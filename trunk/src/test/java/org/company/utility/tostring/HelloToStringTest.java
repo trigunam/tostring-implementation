447e9bee8d7a0f071648e4f602ef8f78fb7366fc
triguna.mutuguppe@gmail.com <triguna.mutuguppe@gmail.com@d388b899-91f9-6f53-1d5f-1aac033a689c>
2012-09-08 08:16:49 +0000

    First version of project. Contains code to explain the HelloToString a POJO, toString implementation defined in Utilities class and a test class to test the toString implementation for HelloToString POJO. This project uses slf4j over logback library to print the logs in a log file called tsi.log. Also this project runs under maven.

---
+++ trunk/src/test/java/org/company/utility/tostring/HelloToStringTest.java
@@ -0,0 +1,63 @@
+package org.company.utility.tostring;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Test class to test the to-string implementation.
+ * 
+ * @author Triguna
+ * @since Project v1.0
+ */
+public class HelloToStringTest
+{
+	private static final Logger _logger = LoggerFactory.getLogger(HelloToStringTest.class);
+
+	HelloToString helloToString;
+
+	/**
+	 * @return the Logger
+	 */
+	public static Logger getLogger()
+	{
+		return _logger;
+	}
+
+	/**
+	 * @param helloToString
+	 *            the helloToString to set
+	 */
+	public void setHelloToString(HelloToString helloToString)
+	{
+		this.helloToString = helloToString;
+	}
+
+	/**
+	 * @return the helloToString
+	 */
+	public HelloToString getHelloToString()
+	{
+		return this.helloToString;
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception
+	{
+		this.setHelloToString(new HelloToString("Triguna", "Bangalore, Karnataka, India"));
+	}
+
+	/**
+	 * Test method for {@link org.company.utility.tostring.HelloToString#toString()}.
+	 */
+	@Test
+	public void testToString()
+	{
+		// Using slf4j over logback to print the toString implementation for HelloToString.
+		getLogger().debug("{}", this.getHelloToString().toString());
+	}
+}
